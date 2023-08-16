914de5c75b7d23ae0000e2bbf580b8c749743a24
Андрій Грицюк <venganpro12@gmail.com>
2023-08-16 12:33:02 +0300

    q

---
+++ Data Access #2/SQL scripts/ADO.NET_creates.sql
@@ -0,0 +1,31 @@
+﻿create database Sells
+go
+
+use Sells
+go
+
+create table Customers
+(
+	Id int primary key identity,
+	Name nvarchar(100) not null check(Name <> ''),
+	Surname nvarchar(100) not null check(Surname <> '')
+);
+go
+
+create table Sellers
+(
+	Id int primary key identity,
+	Name nvarchar(100) not null check(Name <> ''),
+	Surname nvarchar(100) not null check(Surname <> '')
+);
+go
+
+create table TSells
+(
+	Id int primary key identity,
+	CustomerId int not null references Customers(Id),
+	SellerId int not null references Sellers(Id),
+	Price money not null check(Price > 0),
+	Date date not null check(Date < GETDATE())
+);
+go
---
+++ Data Access #2/SQL scripts/ADO.NET_inserts.sql
@@ -0,0 +1,68 @@
+-- Customers Table Inserts
+INSERT INTO Customers (Name, Surname) VALUES ('John', 'Doe');
+INSERT INTO Customers (Name, Surname) VALUES ('Jane', 'Smith');
+INSERT INTO Customers (Name, Surname) VALUES ('Michael', 'Johnson');
+INSERT INTO Customers (Name, Surname) VALUES ('Emily', 'Brown');
+INSERT INTO Customers (Name, Surname) VALUES ('Robert', 'Lee');
+INSERT INTO Customers (Name, Surname) VALUES ('Mary', 'Williams');
+INSERT INTO Customers (Name, Surname) VALUES ('David', 'Jones');
+INSERT INTO Customers (Name, Surname) VALUES ('Sarah', 'Miller');
+INSERT INTO Customers (Name, Surname) VALUES ('William', 'Davis');
+INSERT INTO Customers (Name, Surname) VALUES ('Laura', 'Taylor');
+INSERT INTO Customers (Name, Surname) VALUES ('James', 'Wilson');
+INSERT INTO Customers (Name, Surname) VALUES ('Karen', 'Anderson');
+INSERT INTO Customers (Name, Surname) VALUES ('Daniel', 'Martinez');
+INSERT INTO Customers (Name, Surname) VALUES ('Jessica', 'Garcia');
+INSERT INTO Customers (Name, Surname) VALUES ('Christopher', 'Lopez');
+INSERT INTO Customers (Name, Surname) VALUES ('Linda', 'Hernandez');
+INSERT INTO Customers (Name, Surname) VALUES ('Matthew', 'Gonzalez');
+INSERT INTO Customers (Name, Surname) VALUES ('Patricia', 'Perez');
+INSERT INTO Customers (Name, Surname) VALUES ('Richard', 'Sanchez');
+INSERT INTO Customers (Name, Surname) VALUES ('Jennifer', 'Rivera');
+go
+
+-- Sellers Table Inserts
+INSERT INTO Sellers (Name, Surname) VALUES ('Alex', 'Johnson');
+INSERT INTO Sellers (Name, Surname) VALUES ('Emily', 'Smith');
+INSERT INTO Sellers (Name, Surname) VALUES ('Michael', 'Williams');
+INSERT INTO Sellers (Name, Surname) VALUES ('Sophia', 'Brown');
+INSERT INTO Sellers (Name, Surname) VALUES ('Ethan', 'Martinez');
+INSERT INTO Sellers (Name, Surname) VALUES ('Olivia', 'Lee');
+INSERT INTO Sellers (Name, Surname) VALUES ('Daniel', 'Taylor');
+INSERT INTO Sellers (Name, Surname) VALUES ('Ava', 'Jones');
+INSERT INTO Sellers (Name, Surname) VALUES ('William', 'Garcia');
+INSERT INTO Sellers (Name, Surname) VALUES ('Isabella', 'Miller');
+INSERT INTO Sellers (Name, Surname) VALUES ('James', 'Hernandez');
+INSERT INTO Sellers (Name, Surname) VALUES ('Mia', 'Gonzalez');
+INSERT INTO Sellers (Name, Surname) VALUES ('Benjamin', 'Lopez');
+INSERT INTO Sellers (Name, Surname) VALUES ('Sophia', 'Perez');
+INSERT INTO Sellers (Name, Surname) VALUES ('Jacob', 'Sanchez');
+INSERT INTO Sellers (Name, Surname) VALUES ('Emma', 'Rivera');
+INSERT INTO Sellers (Name, Surname) VALUES ('Michael', 'Anderson');
+INSERT INTO Sellers (Name, Surname) VALUES ('Ava', 'Davis');
+INSERT INTO Sellers (Name, Surname) VALUES ('Matthew', 'Wilson');
+INSERT INTO Sellers (Name, Surname) VALUES ('Olivia', 'Martin');
+go
+
+-- TSells Table Inserts
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (1, 5, 100.50, '2023-07-01');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (2, 4, 75.20, '2023-07-02');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (3, 3, 120.00, '2023-07-03');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (4, 2, 200.00, '2023-07-04');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (5, 1, 350.75, '2023-07-05');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (6, 5, 50.30, '2023-07-06');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (7, 4, 180.90, '2023-07-07');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (8, 3, 95.25, '2023-07-08');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (9, 2, 300.10, '2023-07-09');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (10, 1, 220.50, '2023-07-10');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (11, 5, 180.20, '2023-07-11');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (12, 4, 65.75, '2023-07-12');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (13, 3, 150.00, '2023-07-13');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (14, 2, 250.25, '2023-07-14');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (15, 1, 175.60, '2023-07-15');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (16, 5, 80.40, '2023-07-16');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (17, 4, 135.00, '2023-07-17');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (18, 3, 210.70, '2023-07-18');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (19, 2, 120.90, '2023-07-19');
+INSERT INTO TSells (CustomerId, SellerId, Price, Date) VALUES (20, 1, 90.25, '2023-07-19');
+go
