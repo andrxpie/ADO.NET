132cbdea582fd3bfda0a07c73f820911ac5c3ebc
Андрій Грицюк <venganpro12@gmail.com>
2023-08-16 20:32:45 +0300

    q

--- Entity Framework Intro/Entities/Category.cs
+++ Entity Framework Intro/Entities/Category.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public ICollection<Category> Categories { get; set; }
+        public ICollection<Playlist> Playlists { get; set; }
     }
 }
