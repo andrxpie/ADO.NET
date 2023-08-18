e172afa6839c014d114d9b10ef3816dde43fd844
Андрій Грицюк <venganpro12@gmail.com>
2023-08-18 18:07:24 +0300

    added next lection

---
+++ EF - Migrations. Relations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EF___Migrations._Relations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
