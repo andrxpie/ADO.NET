e19339a2b54cd2a5e5bb748c384bb2a010645557
Андрій Грицюк <venganpro12@gmail.com>
2023-08-23 18:28:30 +0300

    q

--- Fluent API/Entities/City.cs
+++ Fluent API/Entities/City.cs
@@ -8,18 +8,14 @@ using System.Threading.Tasks;
 
 namespace Loadings
 {
-    public class Shop
+    public class City
     {
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Address { get; set; }
 
-        [ForeignKey("City")]
-        public int CityId { get; set; }
-        public City City { get; set; }
-        public int? ParkingArea { get; set; }
-        ICollection<Product> Products { get; set; }
-        ICollection<Worker> Workers { get; set; }
+        [ForeignKey("Country")]
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
     }
 }
