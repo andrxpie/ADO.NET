6ac1888d22d6b04806968aee078da55446c146f8
Андрій Грицюк <venganpro12@gmail.com>
2024-01-04 22:31:40 +0200

    Loadings hw.

--- Loadings/Program.cs
+++ Loadings/Program.cs
@@ -6,7 +6,19 @@
         {
             ShopDB shopDB = new ShopDB();
 
+            Console.WriteLine("ID\t\tName\t\tCountryID");
+            foreach (var city in shopDB.Cities.ToList()) // Eagerly loading ??
+                Console.WriteLine($"{city.Id}\t\t{city.Name}\t\t{city.CountryId}");
 
+            Console.WriteLine("ID\t\tName");
+            foreach (var category in shopDB.Categories) // Lazy loading
+                Console.WriteLine($"{category.Id}\t\t{category.Name}");
+
+            using (shopDB) // Explicit loading
+            {
+                var post = shopDB.Workers.Find("James");
+                shopDB.Entry(post).Reference(p => p.Name).Load();
+            }
         }
     }
 }
